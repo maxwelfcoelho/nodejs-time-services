54c03855c61106315b36577ad1bfcc1e84229929
Maxwel <maxwelfcoelho@gmail.com>
2021-08-04 15:44:49 -0300

    implement the api service and fix a typo inside the unit tests.

---
+++ src/services/TimeService.js
@@ -0,0 +1,22 @@
+const supportedTimezones = new Map();
+supportedTimezones.set('Ireland', 'Europe/Dublin');
+supportedTimezones.set('Brazil', 'America/Sao_Paulo');
+
+class TimeService {
+    constructor() {}
+    
+    getGMTTime() {
+        const time = new Date();
+        return `${time.getUTCHours()}:${time.getUTCMinutes()}:${time.getUTCSeconds()}`;
+    }
+
+    getTimeAtTimeZone(timezone) {
+        if (!timezone || typeof timezone !== 'string') {
+            timezone = supportedTimezones.get('Brazil');
+        }
+        const time = new Date(new Date().toLocaleString('pt-BR', {timeZone: timezone}));
+        return `${time.getHours()}:${time.getMinutes()}:${time.getUTCSeconds()}`;
+    }
+}
+
+module.exports = TimeService;
