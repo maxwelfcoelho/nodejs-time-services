0ad1aff4538bd8bcc8e2b41047ee62912f1c8c60
Maxwel <maxwelfcoelho@gmail.com>
2021-08-07 20:57:39 -0300

    add unit tests for time operation method

--- test/unit/services/timeService.test.js
+++ test/unit/services/timeService.test.js
@@ -46,4 +46,68 @@ describe('time service', () => {
         const result = timeService.getTimeAtTimeZone(120192);
         expect(result).toEqual(expected);
     });
+
+    test('add seconds to gmt timezone', () => {
+        const time = timeService.getGMTTime();
+        expect(timeService.timeOperation(time, 'add', 10, 'seconds')).toEqual('18:7:20');
+        expect(timeService.timeOperation(time, 'add', 25, 'seconds')).toEqual('18:7:35');
+    });
+
+    test('add seconds to gmt-3 timezone', () => {
+        const time = timeService.getTimeAtTimeZone('America/Sao_Paulo');
+        expect(timeService.timeOperation(time, 'add', 10, 'seconds')).toEqual('15:7:20');
+        expect(timeService.timeOperation(time, 'add', 25, 'seconds')).toEqual('15:7:35');
+    });
+
+    test('add seconds to ist timezone', () => {
+        const time = timeService.getTimeAtTimeZone('Europe/Dublin');
+        expect(timeService.timeOperation(time, 'add', 10, 'seconds')).toEqual('19:7:20');
+        expect(timeService.timeOperation(time, 'add', 25, 'seconds')).toEqual('19:7:35');
+    });
+
+    test('subtract seconds to gmt timezone', () => {
+        const time = timeService.getGMTTime();
+        expect(timeService.timeOperation(time, 'sub', 10, 'seconds')).toEqual('18:7:0');
+        expect(timeService.timeOperation(time, 'sub', 25, 'seconds')).toEqual('18:6:45');
+    });
+
+    test('subtract seconds to gmt-3 timezone', () => {
+        const time = timeService.getTimeAtTimeZone('America/Sao_Paulo');
+        expect(timeService.timeOperation(time, 'sub', 10, 'seconds')).toEqual('15:7:0');
+        expect(timeService.timeOperation(time, 'sub', 25, 'seconds')).toEqual('15:6:45');
+    });
+
+    test('subtract seconds to ist timezone', () => {
+        const time = timeService.getTimeAtTimeZone('Europe/Dublin');
+        expect(timeService.timeOperation(time, 'sub', 10, 'seconds')).toEqual('19:7:0');
+        expect(timeService.timeOperation(time, 'sub', 25, 'seconds')).toEqual('19:6:45');
+    });
+
+    test('add minutes to gmt timezone', () => {
+        const time = timeService.getGMTTime();
+        expect(timeService.timeOperation(time, 'add', 10, 'minutes')).toEqual('18:17:10');
+        expect(timeService.timeOperation(time, 'add', 30, 'minutes')).toEqual('18:37:10');
+    });
+
+    test('add minutes to gmt-3 timezone', () => {
+        const time = timeService.getTimeAtTimeZone('America/Sao_Paulo');
+        expect(timeService.timeOperation(time, 'add', 10, 'minutes')).toEqual('15:17:10');
+        expect(timeService.timeOperation(time, 'add', 25, 'minutes')).toEqual('15:32:10');
+    });
+
+    test('add minutes to ist timezone', () => {
+        const time = timeService.getTimeAtTimeZone('Europe/Dublin');
+        expect(timeService.timeOperation(time, 'add', 12, 'minutes')).toEqual('19:19:10');
+        expect(timeService.timeOperation(time, 'add', 48, 'minutes')).toEqual('19:55:10');
+    });
+
+    test('should throw an error when given invalid operation', () => {
+        const time = timeService.getGMTTime();
+        expect(() => timeService.timeOperation(time, 'test', 12, 'minutes')).toThrow('Invalid operation');
+    });
+
+    test('should throw an error when given invalid time unit', () => {
+        const time = timeService.getGMTTime();
+        expect(() => timeService.timeOperation(time, 'add', 7, 'concat')).toThrow('Invalid time unit');
+    });
 });
