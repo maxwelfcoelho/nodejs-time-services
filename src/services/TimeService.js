7fd4cd25a608a4cb02b692abb27271ae2c543e3a
Maxwel <maxwelfcoelho@gmail.com>
2021-08-07 21:00:12 -0300

    time operation method implementation

--- src/services/TimeService.js
+++ src/services/TimeService.js
@@ -17,6 +17,53 @@ class TimeService {
         const time = new Date(new Date().toLocaleString('pt-BR', {timeZone: timezone}));
         return `${time.getHours()}:${time.getMinutes()}:${time.getUTCSeconds()}`;
     }
+
+    timeOperation(time, operation, value, timeUnit) {
+        const date = this.timeToDate(time);
+    
+        let newDate;
+        if (operation === 'add') {
+            newDate = this.addToDateTime(date, timeUnit, value);
+        } else if (operation === 'sub') {
+            newDate = this.subtractToDateTime(date, timeUnit, value);
+        } else {
+            throw new Error('Invalid operation');
+        }
+
+        return `${newDate.getHours()}:${newDate.getMinutes()}:${newDate.getSeconds()}`;
+    }
+
+    timeToDate(time) {
+        const splittedTime = time.split(':');
+        const hours = parseInt(splittedTime[0]);
+        const minutes = parseInt(splittedTime[1]);
+        const seconds = parseInt(splittedTime[2]);
+        const today = new Date();
+    
+        return new Date(today.getFullYear(), today.getMonth(), today.getDay(), hours, minutes, seconds);
+    }
+
+    addToDateTime(date, timeUnit, value) {
+        if (timeUnit === 'seconds') {
+             date.setSeconds(date.getSeconds() + parseInt(value));
+        } else if (timeUnit === 'minutes') {
+            date.setMinutes(date.getMinutes() + parseInt(value));
+        } else {
+            throw new Error('Invalid time unit');
+        }
+        return date;
+    }
+
+    subtractToDateTime(date, timeUnit, value) {
+        if (timeUnit === 'seconds') {
+            date.setSeconds(date.getSeconds() - parseInt(value));
+        } else if (timeUnit === 'minutes') {
+            date.setMinutes(date.getMinutes() - parseInt(value));
+        } else {
+            throw new Error('Invalid time unit');
+        }
+        return date;
+    }
 }
 
 module.exports = {
