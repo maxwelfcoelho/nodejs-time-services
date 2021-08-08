24e5655b88d28b5b413e19b9ca0c02b7965ca8b9
Maxwel <maxwelfcoelho@gmail.com>
2021-08-08 11:36:30 -0300

    time operation endpoint implementation

--- src/app.js
+++ src/app.js
@@ -5,5 +5,9 @@ const app = express();
 const TimeController = require('./controllers/TimeController');
 
 app.get('/time/current', TimeController.get);
+app.get('/time/:operation/:value/:timeUnit', TimeController.timeOperation);
+app.get('/test/:id', (req, res) => {
+    res.status(200).json(req.params.id);
+});
 
 module.exports = app;
--- src/controllers/TimeController.js
+++ src/controllers/TimeController.js
@@ -3,7 +3,7 @@ const { TimeService, supportedTimezones } = require('../services/TimeService');
 class TimeController {
     constructor() {}
 
-    get(req, res) {
+    async get(req, res) {
         const timeService = new TimeService();
         const times = {
             'GMT': timeService.getGMTTime(),
@@ -12,6 +12,24 @@ class TimeController {
         };
 
         res.status(200).json(times)
+    }
+
+    async timeOperation(req, res) {
+        try {
+            const timeService = new TimeService();
+            const { operation, value, timeUnit } = req.params;
+            const gmtTime = timeService.getGMTTime();
+            const gmt3Time = timeService.getTimeAtTimeZone(supportedTimezones.get('Brazil'));
+            const istTime = timeService.getTimeAtTimeZone(supportedTimezones.get('Ireland'));
+
+            res.status(200).json({
+                'GMT': timeService.timeOperation(gmtTime, operation, value, timeUnit),
+                'GMT-3': timeService.timeOperation(gmt3Time, operation, value, timeUnit),
+                'IST': timeService.timeOperation(istTime, operation, value, timeUnit)
+            });
+        } catch(err) {
+            res.status(400).json(err.message);
+        }
     }
 }
 
