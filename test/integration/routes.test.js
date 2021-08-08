34d786603233564653e6381007af41f7e91b7baa
Maxwel <maxwelfcoelho@gmail.com>
2021-08-08 11:35:32 -0300

    add integration tests to time operation

--- test/integration/routes.test.js
+++ test/integration/routes.test.js
@@ -37,4 +37,61 @@ describe('get /time/current', () => {
             })
             .catch(err => done(err));
     });
+
+    test('should add seconds to the supported times', done => {
+        const endpoint = `/time/add/12/seconds`;
+        supertest(app)
+            .get(endpoint)
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .then(response => {
+                expect(response.body).toEqual({ 'GMT': '18:7:22', 'GMT-3': '15:7:22', 'IST': '19:7:22' });
+                done();
+            })
+            .catch(err => done(err));
+    });
+
+    test('should add minutes to the supported times', done => {
+        const endpoint = `/time/add/36/minutes`;
+        supertest(app)
+            .get(endpoint)
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .then(response => {
+                expect(response.body).toEqual({ 'GMT': '18:43:10', 'GMT-3': '15:43:10', 'IST': '19:43:10' });
+                done();
+            })
+            .catch(err => done(err));
+    });
+
+    test('invalid operation throws an error', done => {
+        const endpoint = `/time/calculate/36/minutes`;
+        supertest(app)
+            .get(endpoint)
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(400)
+            .then(response => {
+                expect(response.body).toEqual('Invalid operation');
+                done();
+            })
+            .catch(err => done(err));
+    });
+
+    test('invalid time unit throws an error', done => {
+        const endpoint = `/time/add/36/halfsecond`;
+        supertest(app)
+            .get(endpoint)
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(400)
+            .then(response => {
+                expect(response.body).toEqual('Invalid time unit');
+                done();
+            })
+            .catch(err => done(err));
+    });
+
 });
